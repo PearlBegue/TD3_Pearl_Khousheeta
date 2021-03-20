de4356dbac09f7e1168581cda7bdc14d8e0d885f
PearlBegue <pearlbeg.25@gmail.com>
2021-03-20 22:54:59 +0400

    [add] utiliser input pour laddition

--- addTowlnt.py
+++ addTowlnt.py
@@ -3,13 +3,8 @@ def add_num(a,b):
     sum=a+b;
     return sum;
 def main():
-    num1= int (2);
-    num2= int (3);
-    num3= int (1);
-    number=(num1,num2,num3);
-    calculate=int(add_num(num1,num2))
-    if (number != 2) :
-        print("Number 1 is :",num1," Number 2 is :",num2,"Number 3 is",num3," and Sum is",calculate);
-    else:
-        print("Error Try again, 3 arguements");
+    num1= int(input("Enter the number 1: "));
+    num2= int(input("Enter the number 2: "));
+    calculate=int(add_num(num1,num2));
+    print("Number 1 is :",num1," Number 2 is :",num2," and Sum is",calculate);
 main()
