9a9e548d8b1d0f034f1c4228fdbd5168d27bdf35
PearlBegue <pearlbeg.25@gmail.com>
2021-03-20 22:11:16 +0400

    fs

---
+++ addTowlnt.py
@@ -0,0 +1,10 @@
+#!/bin/bash
+def add_num(a,b):
+    sum=a+b;
+    return sum;
+def main():
+    num1= int (2);
+    num2= int (3);
+    calculate=int(add_num(num1,num2));
+    print("Number 1 is :",num1," Number 2 is :",num2," and Sum is",calculate)
+main()
