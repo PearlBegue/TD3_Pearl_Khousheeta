0b9e8a3f72b8461cd558ec46c34dcd9a041da0c9
PearlBegue <pearlbeg.25@gmail.com>
2021-03-20 22:47:08 +0400

    [add]ajout un messaged d'erreurr

--- addTowlnt.py
+++ addTowlnt.py
@@ -5,6 +5,11 @@ def add_num(a,b):
 def main():
     num1= int (2);
     num2= int (3);
-    calculate=int(add_num(num1,num2));
-    print("Number 1 is :",num1," Number 2 is :",num2," and Sum is",calculate)
+    num3= int (1);
+    number=(num1,num2,num3);
+    calculate=int(add_num(num1,num2))
+    if (number != 2) :
+        print("Number 1 is :",num1," Number 2 is :",num2,"Number 3 is",num3," and Sum is",calculate);
+    else:
+        print("Error Try again, 3 arguements");
 main()
