ee749010c8389697ec4b8507c0a56bb80497418b
PearlBegue <79831418+PearlBegue@users.noreply.github.com>
2021-03-21 19:15:24 +0400

    Update addTowlnt.py

--- addTowlnt.py
+++ addTowlnt.py
@@ -3,8 +3,26 @@ def add_num(a,b):
     sum=a+b;
     return sum;
 def main():
-    num1= int(input("Enter the number 1: "));
-    num2= int(input("Enter the number 2: "));
-    calculate=int(add_num(num1,num2));
-    print("Number 1 is :",num1," Number 2 is :",num2," and Sum is",calculate);
+    import sys
+    print(sys.argv)
+    i=(len(sys.argv)-1)
+    print("Amount of arguement:",i)
+
+    if (i==0):
+        num1=int(input("Enter the number 1:"));
+        num2=int(input("Enter the number 2:"));
+        x=int(num1);
+        y=int(num2);
+        print("The sum is ",add_num(num1,num2));
+    elif (i==1):
+        n1=int(input("Enter the second number: "));
+        x=int(sys.argv[1]);
+        y=int(num1);
+        print("The sum is ",add_num(x,y));
+    elif (i==2):
+        x=int(sys.argv[1])
+        y=int(sys.argv[2])
+        print("The sum is ",add_num(x,y));
+    else:
+        print ("Error: Please Insert only two arguement")
 main()
